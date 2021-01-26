be0ea8656aae0facf9553602a078f52729817233
Louis <louis@jurczyk.fr>
2021-01-26 09:55:57 +0100

    Update __init__.py

--- opulence/engine/__init__.py
+++ opulence/engine/__init__.py
@@ -1,26 +1,13 @@
 from config import engine_config
-from celery import Celery
-from kombu.serialization import register
-from opulence.common import json_encoder
 from opulence.common.database.es import create_client
+from opulence.common.celery import create_celery_app
 
-register(
-    "customEncoder",
-    json_encoder.json_dumps,
-    json_encoder.json_loads,
-    content_type="application/x-customEncoder",
-    content_encoding="utf-8",
-)
-
-celery_app = Celery(__name__)
+celery_app = create_celery_app()
 celery_app.conf.update(
     {
         'task_routes': {
-                # 'scan.*': { 'queue': 'scan', 'exchange': 'scan' }
-        },
-        "accept_content": ["customEncoder", "application/json"],
-        "task_serializer": "customEncoder",
-        "result_serializer": "customEncoder",
+                'toto.*': { 'queue': 'toto', 'exchange': 'toto' }
+        }
     }
 )
 celery_app.conf.update(engine_config.celery)
