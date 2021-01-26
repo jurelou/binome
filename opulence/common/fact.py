251d72f55a17136737f4db12cb7e17b28adb958d
Louis <louis@jurczyk.fr>
2021-01-26 15:14:34 +0100

    Update fact.py

--- opulence/common/fact.py
+++ opulence/common/fact.py
@@ -3,6 +3,17 @@ from pydantic import BaseModel
 
 
 class BaseFact(BaseModel):
+    __hash: Optional[str] = None
+
+    @root_validator
+    def set_hash(cls, values):
+        values.pop("__hash", None)
+        m = hashlib.sha256()
+        for k in sorted(values):
+            m.update(str(k).encode() + str(values[k]).encode())
+        values["__hash"] = m.hexdigest()
+        return values
+ 
     class Config(BaseConfig):
         allow_population_by_alias = True
         extra = "allow"
