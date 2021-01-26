1ed29e6296bbc50362e3d3533b7d7697a44e0d79
Louis <louis@jurczyk.fr>
2021-01-26 16:20:02 +0100

    Update facts.py

--- opulence/common/database/es/facts.py
+++ opulence/common/database/es/facts.py
@@ -1,5 +1,5 @@
 from opulence.common.database.es.base import BaseIndex
-
+from elasticsearch.helpers import parallel_bulk
 
 class FactIndex(BaseIndex):
   index_name = "facts"
@@ -7,3 +7,19 @@ class FactIndex(BaseIndex):
   @property
   def mapping(self):
     return {"mappings": {"properties": {}}}
+  
+  
+  def bulk_upsert(self, es_client, actions):
+    actions = [{'username':'Tom', "id": 1}]
+    
+    def gen_actions(actions):
+      for action in actions:
+        _id = action.pop("id")
+        yield {
+            '_op_type': 'update',
+            '_index': self.index_name,
+            '_id': _id,
+            'doc': action,
+            "doc_as_upsert":True # ????
+        }
+    es.parallel_bulk(client=es_client, actions=gen_actions(actions))
