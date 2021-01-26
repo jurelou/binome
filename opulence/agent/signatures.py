e26122a772d3876ef507fd907ad1d24057c55f95
Louis <louis@jurczyk.fr>
2021-01-26 12:11:30 +0100

    Update signatures.py

--- opulence/agent/signatures.py
+++ opulence/agent/signatures.py
@@ -1,6 +1,7 @@
-from opulence.agent import tasks
+from opulence.agent import celery_app 
 from opulence.common.base_fact import BaseFact
 from typing import List
 
 def launch_scan(collector_name: str, facts: List[BaseFact]):
+    return celery_app.signature("scan.launch", immutable=True)
     return tasks.launch_scan.signature(args=[collector_name, facts])
