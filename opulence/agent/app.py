c4161730b96d251e3b5b5f61401680af7fade021
Louis <louis@jurczyk.fr>
2021-01-26 12:04:44 +0100

    Update app.py

--- opulence/agent/app.py
+++ opulence/agent/app.py
@@ -3,7 +3,7 @@ from celery.signals import worker_init
 from config import engine_config
 from opulence.agent import celery_app
 from opulence.common.base_fact import BaseFact
-from opulence.agent.scan import tasks
+from opulence.agent import tasks
 from opulence.common.database.es import create_indexes
 
 # @celery_app.task(name="scan.launch")
@@ -18,4 +18,3 @@ def startup(sender=None, conf=None, **kwargs):
         create_indexes(engine_config.elasticsearch.endpoint)
     except Exception as err:
         print(f"Error while bootstraping elasticsearch: {err}")
-
