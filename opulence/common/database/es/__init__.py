22a083caafe444e24233f3223861dc20d57850cf
Louis <louis@jurczyk.fr>
2021-01-26 10:04:21 +0100

    Update __init__.py

--- opulence/common/database/es/__init__.py
+++ opulence/common/database/es/__init__.py
@@ -1,6 +1,4 @@
 from opulence.common.database.es import facts_index
-import httpx
-from config import engine_config
 from elasticsearch import Elasticsearch
 
 
@@ -21,17 +19,6 @@ def create_indexes(es_client):
             print(f"Index {index.index_name} already exists")
     print("done")
 
-def create_kibana_index_patterns():
-    for index in INDEXES:
-        kibana_endpoint = f"{engine_config.kibana.url}/api/saved_objects/index-pattern/{index.index_name}"
-        headers = {"kbn-xsrf": "yes", "Content-Type": "application/json"}
-        data = {
-            "attributes": {
-                "title": f"{index.index_name}*"
-            }
-        }
-        r = httpx.post(kibana_endpoint, json=data, headers=headers)
-        print(f"Kibana create index pattern: {r.status_code}")
 
 # def clean(es_client):
 #     for index in indexes:
