ed5b04bc318658050e7d9237f43d1cee4981403e
Louis <louis@jurczyk.fr>
2021-01-26 10:14:17 +0100

    Update facts_index.py

--- opulence/common/database/es/facts_index.py
+++ opulence/common/database/es/facts_index.py
@@ -1,17 +1,9 @@
-index_name = "facts"
+from opulence.common.database.es.base_index import BaseIndex
 
-def get_elastic_mapping():
-        return {"mappings": {"properties": {}}}
 
-def create_index(es_client):
-    es_client.indices.create(index=index_name)
-    es_client.indices.put_settings(
-        index=index_name,
-        body={"refresh_interval": "30s", "number_of_replicas": 0},
-    )
+class FactIndex(BaseIndex):
+  index_name = "facts"
 
-def remove_index(es_client):
-    es_client.indices.delete(index=index_name)
-
-def toto():
-    pass
+  @property
+  def mapping(self):
+    return {"mappings": {"properties": {}}}
