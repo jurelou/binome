ea8a6612416a7f35c9c438d1977357f8d1e2f51b
Louis <louis@jurczyk.fr>
2021-01-26 11:59:46 +0100

    Update tasks.py

--- opulence/engine/tasks.py
+++ opulence/engine/tasks.py
@@ -1,1 +1,19 @@
+from opulence.engine import celery_app
+from opulence.agent.scan import tasks
+from opulence.agent.scan import signatures
+from opulence.facts.person import Person
+from opulence.common.base_fact import BaseFact
 
+
+# a = tasks.test_agent.apply_async().get()
+# print("@@@@@@@@@@@@@@@@@@@")
+# print("res->", a)
+# print("@@@@@@@@@@@@@@@@@")
+
+# @celery_app.task(name="engine.toto")
+# def single_collector_scan(collector_name: str, facts: List[BaseFact]):
+#     print("!!!!!!ENGINE TOTO")
+
+
+# a = signatures.launch_scan("a-collector", [Person(firstname="a", lastname="b")]).apply_async()
+# print("RESYULT", a.get())
