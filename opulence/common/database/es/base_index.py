9992a207d6a20a45086284bd80061f696366124f
Louis <louis@jurczyk.fr>
2021-01-26 10:12:18 +0100

    Update base_index.py

--- opulence/common/database/es/base_index.py
+++ opulence/common/database/es/base_index.py
@@ -1,2 +1,21 @@
+
 class BaseIndex():
   index_name = None
+  replicas = 0
+  refresh_interval = "30s"
+
+  @property
+  def mapping(self):
+    return {"mappings": {"properties": {}}}
+
+ 
+  def create_index(self, es_client):
+      es_client.indices.create(index=self.index_name)
+      es_client.indices.put_settings(
+          index=self.index_name,
+          body={"refresh_interval": self.refresh_interval, "number_of_replicas": self.replicas},
+      )
+
+  def remove_index(self, es_client):
+      es_client.indices.delete(index=self.index_name)
+
