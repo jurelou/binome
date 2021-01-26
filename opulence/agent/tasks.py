356b61375096b9485732969be10b48f7196ded39
Louis <louis@jurczyk.fr>
2021-01-26 14:56:36 +0100

    Update tasks.py

--- opulence/agent/tasks.py
+++ opulence/agent/tasks.py
@@ -14,7 +14,8 @@ def test_agent():
     # for i in [1, 2, 3, 4]:
     #     yield i
 
-@celery_app.task(throws=(exceptions.BaseAgentException), name="scan.launch", acks_late=True)
+#@celery_app.task(throws=(exceptions.BaseAgentException), name="scan.launch", acks_late=True)
+@celery_app.task(throws=(exceptions.BaseAgentException), name="scan.launch")
 def launch_scan(collector_name: str, facts: List[BaseFact]):
 
     print(COLLECTORS)
@@ -24,13 +25,7 @@ def launch_scan(collector_name: str, facts: List[BaseFact]):
     if not COLLECTORS[collector_name]["active"]:
         raise exceptions.CollectorDisabled(f"Collector {collector_name} is not enabled.")
 
-    try:
-        COLLECTORS[collector_name]["instance"].collect(facts)
-    except exceptions.CollectorRuntimeError as err:
-        print("->", err)
-
-
-    print(collector_name in COLLECTORS)
-    print("!!!!!!JESCAcool", collector_name)
-    print("=====", facts)
-    print("--", COLLECTORS)
+    
+    result = COLLECTORS[collector_name]["instance"].collect(facts)
+    result = result.dict()
+    return result
