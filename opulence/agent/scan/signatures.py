3aecb21cd28e6641b6d04c53d64907facba2fceb
Louis <louis@jurczyk.fr>
2021-01-26 14:03:29 +0100

    Delete opulence/agent/scan directory

--- opulence/agent/scan/signatures.py
+++
@@ -1,6 +0,0 @@
-from opulence.agent.scan import tasks
-from opulence.common.base_fact import BaseFact
-from typing import List
-
-def launch_scan(collector_name: str, facts: List[BaseFact]):
-    return tasks.launch_scan.signature(args=[collector_name, facts])
