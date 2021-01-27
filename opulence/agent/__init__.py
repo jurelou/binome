84cd75811e7cf962864f7123f418a5240960ce91
Louis <louis@jurczyk.fr>
2021-01-27 09:47:22 +0100

    Update __init__.py

--- opulence/agent/__init__.py
+++ opulence/agent/__init__.py
@@ -1,34 +1,16 @@
 from config import agent_config
 
-from opulence.agent.collector.base import BaseCollector
-from opulence.common.utils import load_classes_from_module
-from opulence.common.exceptions import InvalidCollectorDefinition
 from opulence.common.celery import create_app
 
-def load_collectors():
-    collector_modules = load_classes_from_module(root_path="opulence/agent/collectors", parent_class=BaseCollector, skip_first_level=True)
-    collector_instances = {}
-    for collector in collector_modules:
-        collector_instance = collector()
-        collector_name = collector_instance.config.name
-        if collector_name in collector_instances:
-            raise InvalidCollectorDefinition(f"Found collector with duplicate name `{collector_name}`.")
-        collector_instances[collector_name] = {"instance": collector_instance, "active": False}
-    for collector_name in set(agent_config.collectors or []):
-        if collector_name not in collector_instances:
-            raise InvalidCollectorDefinition(f"Can't find `{collector_name}`, which is defined in the configuration file. Check your settings.yml file `collectors` section.")        
-        collector_instances[collector_name]["active"] = True
-    return collector_instances
+from opulence.agent.collectors import all_collectors
 
-COLLECTORS = load_collectors()
-
-print("COLLECTORS", COLLECTORS)
+print("COLLECTORS", all_collectors)
 
 
 celery_app = create_app()
 celery_app.conf.update(
     {
-        'collectors': {c_name: {"active": c_item["active"], "config": c_item["instance"].config.dict()} for c_name, c_item in COLLECTORS.items()},
+        'collectors': {c_name: {"active": c_item["active"], "config": c_item["instance"].config.dict()} for c_name, c_item in all_collectors.items()},
         'task_routes': {
                 'scan.*': { 'queue': 'scan', 'exchange': 'scan' }
         },
