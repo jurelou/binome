ca3dcd05bce5317c897bdd47e98c30b784d81bfa
Louis <louis@jurczyk.fr>
2021-01-27 12:29:10 +0100

    Update exceptions.py

--- opulence/common/exceptions.py
+++ opulence/common/exceptions.py
@@ -1,22 +1,2 @@
 class BaseOpulenceException(Exception):
     pass
-
-# Collectors exceptions
-class BaseCollectorException(BaseOpulenceException):
-    pass
-
-class InvalidCollectorDefinition(BaseCollectorException):
-    pass
-
-class CollectorRuntimeError(BaseCollectorException):
-    pass
-
-# Agents exceptions
-class BaseAgentException(BaseOpulenceException):
-    pass
-
-class CollectorNotFound(BaseAgentException):
-    pass
-
-class CollectorDisabled(BaseAgentException):
-    pass
