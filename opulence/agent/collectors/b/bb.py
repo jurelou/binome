c3a1effb7bfe17c77580b85672a9d1a9d6d88fa3
Louis <louis@jurczyk.fr>
2021-01-27 09:43:01 +0100

    Update bb.py

--- opulence/agent/collectors/b/bb.py
+++ opulence/agent/collectors/b/bb.py
@@ -1,4 +1,5 @@
-from opulence.common.collector import BaseCollector
+from opulence.agent.collector.base import BaseCollector
+
 from opulence.facts.person import Person
 from opulence.facts.email import Email
 
