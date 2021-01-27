642ba5b75e4343746a6c56d28e67abe3549f257a
Louis <louis@jurczyk.fr>
2021-01-27 12:28:20 +0100

    Create exceptions.py

---
+++ opulence/agent/exceptions.py
@@ -0,0 +1,11 @@
+from opulence.common.exceptions import BaseOpulenceException
+
+# Agents exceptions
+class BaseAgentException(BaseOpulenceException):
+    pass
+
+class CollectorNotFound(BaseAgentException):
+    pass
+
+class CollectorDisabled(BaseAgentException):
+    pass
