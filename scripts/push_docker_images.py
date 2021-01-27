4dd8dcad1f43860f536d9d1a339feed34e153e34
Louis <louis@jurczyk.fr>
2021-01-27 13:39:05 +0100

    Update push_docker_images.py

--- scripts/push_docker_images.py
+++ scripts/push_docker_images.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python3
+import docker
+
+from pathlib import Path
+
+def get_dockerfiles():  
+  return [ path.name for path in Path('./deploy/docker_images').rglob('*.dockerfile') ]
 
