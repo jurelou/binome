3ef66bcecbefb0a29da6d7028b5311556c3da1c6
Louis <louis@jurczyk.fr>
2021-01-27 10:17:19 +0100

    Update docker.py

--- opulence/agent/collectors/docker.py
+++ opulence/agent/collectors/docker.py
@@ -1,1 +1,10 @@
+from opulence.agent.collectors.base import BaseCollector, BaseConfig
 
+class DockerConfig(BaseConfig):
+    image: str
+
+
+class DockerCollector(BaseCollector):
+
+    def configure(self):
+        self.config = DockerConfig(**self.config)
