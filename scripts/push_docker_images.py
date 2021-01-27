7a632be6b641baad980abc2029d33bd8ae688107
Louis <louis@jurczyk.fr>
2021-01-27 14:00:19 +0100

    Update push_docker_images.py

--- scripts/push_docker_images.py
+++ scripts/push_docker_images.py
@@ -3,6 +3,32 @@ import docker
 
 from pathlib import Path
 
-def get_dockerfiles():  
+docker_client = docker.from_env()
+docker_registry_endpoint = "localhost:5000"
+
+
+def get_dockerfiles():
+  #retourner un tuple path; filename
   return [ path.name for path in Path('./deploy/docker_images').rglob('*.dockerfile') ]
 
+def build_dockerfile(path, filename):
+  image, _ = docker_client.images.build(path=path, dockerfile=filename, tag="latest", rm=True)
+  return image
+  
+def push_docker_image(image_name):
+  image, _ = docker_client.images.push(repository=docker_registry_endpoint, dockerfile=filename, tag="latest", rm=True)
+  return image
+
+def pull_docker_image():
+  pass
+
+def list_images():
+  for image in docker_client.images.list():
+    print(f"-> {image}")
+
+
+for path, filename in get_dockerfiles():
+  print(f"Found dockerfile {dockerfile}")
+  image = build_dockerfile(path, filename)
+  
+list_images()
