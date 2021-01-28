c700e1706be9f6f2f8b64a35c4fa458ffe851b6a
Louis <louis@jurczyk.fr>
2021-01-28 10:43:52 +0100

    Update __init__.py

--- opulence/common/database/es/__init__.py
+++ opulence/common/database/es/__init__.py
@@ -1,25 +1,28 @@
 from opulence.common.database.es.fact import FactIndex
 from elasticsearch import Elasticsearch
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 fact_index = FactIndex()
 INDEXES = [fact_index]
 
 def create_client(config):
-    print(f"Create ES client for config: {config.endpoint}")
+    logger.info(f"Create elasticsearch client: {config.endpoint}")
     return Elasticsearch(hosts=[config.endpoint])
 
 def create_indexes(es_client):
-    print("Bootstraping elasticsearch indexes")
     for index in INDEXES:
         if not es_client.indices.exists(index=index.index_name):
             index.create_index()
-            print("finish")
+            logger.info(f"Created elasticsearch index {index.index_name}")
         else:
-            print(f"Index {index.index_name} already exists")
+            logger.info(f"Index {index.index_name} already exists")
 
 
-# def clean(es_client):
-#     for index in indexes:
-#         if es_client.indices.exists(index=index.index_name):
-#             print(f"Remove index {index.index_name}")
-#             index.remove_index()
+def clean(es_client):
+    for index in indexes:
+        if es_client.indices.exists(index=index.index_name):
+            index.remove_index()
+            logger.info(f"Remove index {index.index_name}")
