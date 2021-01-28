cfccdb27eb0a01386bf0ce3802874c2a73390289
Louis <louis@jurczyk.fr>
2021-01-28 14:42:51 +0100

    Update remote_tasks.py

--- opulence/engine/remote_tasks.py
+++ opulence/engine/remote_tasks.py
@@ -5,4 +5,4 @@ from opulence.agent import tasks
 
 
 def launch_scan(collector_name: str, facts: List[BaseFact]):
-    return celery_app.signature("scan.launch", args=[collector_name, facts])
+    return celery_app.signature("agent.scan.launch", args=[collector_name, facts])
