d0d1df90af7764fa66fc6651955819036d7cb824
Louis <louis@jurczyk.fr>
2021-01-28 14:23:13 +0100

    Update exceptions.py

--- opulence/common/exceptions.py
+++ opulence/common/exceptions.py
@@ -1,2 +1,9 @@
 class BaseOpulenceException(Exception):
     pass
+
+class TaskTimeoutError(BaseOpulenceException):
+    def __init__(self, value=None):
+        self.value = value or ""
+
+    def __str__(self):
+        return "Celery task timeout: ({})".format(self.value)
