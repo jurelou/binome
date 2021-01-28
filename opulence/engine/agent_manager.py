f1d9cd724014faf2b5514c1c8f2669f9d432b4f2
Louis <louis@jurczyk.fr>
2021-01-28 10:31:37 +0100

    Update agent_manager.py

--- opulence/engine/agent_manager.py
+++ opulence/engine/agent_manager.py
@@ -2,7 +2,9 @@ import time
 import threading
 from opulence.engine import celery_app
 
+import logging
 
+logger = logging.getLogger(__name__)
 AGENTS = {}
 
 
@@ -29,35 +31,36 @@ class Manager:
 
     def add_worker(self, worker):
         global AGENTS
-        print("yo")
         inspect = celery_app.control.inspect([worker])
         if not self.is_listening_for_scans(inspect, worker):
+            logger.info(f"Removing worker {worker} (not listening for scans)")
             AGENTS.pop(worker, None)
             return
         conf = inspect.conf()
         if not conf or "collectors" not in conf[worker]:
             AGENTS[worker] = []
+            logger.warn(f"Adding worker {worker} (no configuration found)")
         else:
-            # print(f"ADDING worker {worker} -> {conf[worker]['collectors']}")
+            logger.info(f"Adding worker {worker}")
             AGENTS[worker] = conf[worker]["collectors"]
 
     def offline_event(self, event):
         global AGENTS
-        AGENTS.pop(event["hostname"], None)
-        print(f"AGENTS: {AGENTS}")
+        hostname = event['hostname']
+        AGENTS.pop(hostname, None)
+        logger.info(f"Removing agent {hostname}")
+        logger.debug(f"Active agents: {AGENTS.keys()}")
 
     def online_event(self, event):
-        self.add_worker(event["hostname"])
-        print(f"AGENTS: {AGENTS}")
+        hostname = event['hostname']
+        self.add_worker(hostname)
+        logger.debug(f"Active agents: {AGENTS.keys()}")
 
     def capture(self, handlers, limit):
         with celery_app.connection() as connection:
             recv = celery_app.events.Receiver(connection, handlers=handlers)
             recv.capture(limit=limit, timeout=None, wakeup=True)
-        print("DONE capture")
-        print(f"AGENTS: {AGENTS}")
 
     def run(self):
-        print("go")
         self.capture({'worker-heartbeat': self.online_event}, limit=5)
         self.capture({'worker-online': self.online_event, 'worker-offline': self.offline_event}, limit=None)
